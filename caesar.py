4eb532187cbddc1dc2199b0f492b3d32173e8d52
Heath Naylor <heath@bythepixel.com>
2019-05-20 15:35:06 -0600

    Adjustments for proper indexing

--- caesar.py
+++ caesar.py
@@ -1,43 +1,34 @@
+lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+upper_alphabet = lower_alphabet.upper()
+
 def alphabet_position(letter):
-    lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
-    upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     if letter in lower_alphabet:
-        char_idx= lower_alphabet.find(letter)
-    else:
-        char_idx= upper_alphabet.find(letter)    
-    return char_idx   
+        return lower_alphabet.find(letter)
+ 
+    return upper_alphabet.find(letter)
 
 def rotate_character(char, rot):
-    lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
-    upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    double_lower_alphabet = lower_alphabet * 2
-    double_upper_alphabet = upper_alphabet * 2
-    result = ""
-    char_idx = alphabet_position(char)
-    rot_idx = alphabet_position(rot % 26) 
-    if char in lower_alphabet:
-        result += double_lower_alphabet[rot_idx]
-    elif char in upper_alphabet:
-        result += double_upper_alphabet[rot_idx]
-    else:
-        result += char
-    return result 
+    rot_idx = (alphabet_position(char) + alphabet_position(rot)) % 26
+
+    if char.islower():
+        return lower_alphabet[rot_idx]
+
+    if char.isupper():
+        return upper_alphabet[rot_idx]
+
+    return char
 
 def encrypt(message, key):
-    encrypted_message = []
+    encrypted_message = "" 
     key_idx = [alphabet_position(letter) for letter in key]
     key_len = len(key)
-    message_idx = [alphabet_position(message) for letter in message]
+    message_idx = [alphabet_position(letter) for letter in message]
     for i in range(len(message_idx)):
-        encrypted_message.append(rotate_character(message[i], key[i]))
+        encrypted_message += rotate_character(message[i], key[i % len(key)])
     return encrypted_message
 
 
 print(encrypt("plaintext", "key"))
-
-
-#def main():
-    #print(encrypt())
-
-#if __name__ == "__main__":
-    #main()  
+print(encrypt("PlaintexT", "key"))
+print(encrypt("1PlaintexT", "key"))
+print(encrypt("1Plain texT", "key"))
